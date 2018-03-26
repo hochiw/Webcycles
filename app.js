179cd564c4dec254bbe1f29b79a80042be1c3fc6
TakoYaki <rico5504@gmail.com>
2018-03-26 22:44:30 +1100

    Coming soon page

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ var express = require('express');
 var db = require("./models/db.js");
 var app = express();
 
+app.use(express.static(__dirname));
+app.use(express.static(__dirname + "/css"));
+app.use(express.static(__dirname + "/css/font"));
+
 app.get("/",function(req, res) {
-        res.send("Coming soon");
+        res.sendFile(__dirname + '/index.html');
 });
 
 app.listen('3000');
