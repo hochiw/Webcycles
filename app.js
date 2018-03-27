0547f8ee7461117b4a1e97ab52fb4db77b2e7038
TakoYaki <rico5504@gmail.com>
2018-03-27 17:34:07 +1100

    Fixed Port error

--- app.js
+++ app.js
@@ -10,4 +10,7 @@ app.get("/",function(req, res) {
         res.sendFile(__dirname + '/index.html');
 });
 
-app.listen('3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`App listening to port ${ PORT }`);
+});
