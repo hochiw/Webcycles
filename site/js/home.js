b5511daa76b9953e4c2ca9998bd70f62f673af53
TakoYaki <rico5504@gmail.com>
2018-04-16 11:12:36 +1000

    Updated Title, fixed button overlay size

--- site/js/home.js
+++ site/js/home.js
@@ -1,20 +1,17 @@
 function showLeaderboard(event, type) {
     // Declare all variables
-    var i, tab_ele, tab_btn;
+    var i;
 
-    // Get all elements with class="tabcontent" and hide them
-    tab_ele = document.getElementsByClassName("leaderboardTab");
+    var tab_ele = document.getElementsByClassName("leaderboardTab");
     for (i = 0; i < tab_ele.length; i++) {
         tab_ele[i].style.display = "none";
     }
 
-    // Get all elements with class="tablinks" and remove the class "active"
-    tab_btn = document.getElementsByClassName("tab-btn");
+    var tab_btn = document.getElementsByClassName("tab-btn");
     for (i = 0; i < tab_btn.length; i++) {
         tab_btn[i].className = tab_btn[i].className.replace(" active", "");
     }
 
-    // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(type).style.display = "grid";
     event.currentTarget.className += " active";
 }
