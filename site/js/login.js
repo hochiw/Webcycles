23b33e243e16003193e19c0274f9210ca4f291ca
TakoYaki <rico5504@gmail.com>
2018-04-19 15:21:44 +1000

    added slogan to the login page

--- site/js/login.js
+++ site/js/login.js
@@ -2,17 +2,20 @@
 
 function displayLoginform() {
     document.getElementById("btnContainer").className = "hidden";
+    document.getElementById("slogan").className = "hidden";
     document.getElementById("loginForm").className = "show";
 
 }
 
 function displayRegistrationform() {
     document.getElementById("btnContainer").className = "hidden";
+    document.getElementById("slogan").className = "hidden";
     document.getElementById("registrationForm").className = "show";
 }
 
 function displayChoicebtn() {
     document.getElementById("btnContainer").className = "show";
+    document.getElementById("slogan").className = "show";
     document.getElementById("registrationForm").className = "hidden";
     document.getElementById("loginForm").className = "hidden";
 }
