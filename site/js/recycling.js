f0ab2adb45ab07c4e4b628496731d536833390b2
Kah Chun Lee <kahl2@student.unimelb.edu.au>
2018-05-09 22:02:48 +1000

    Recycling game
    First implementation for recycling game backend in js and html

--- site/js/recycling.js
+++ site/js/recycling.js
@@ -1,21 +1,45 @@
-function input(input,slider,ava) {
+function input(input,slider,total,formName) {
 
-    var pSlider = document.getElementById(slider)
-    var pAva = document.getElementById(ava);
-    pAva.innerHTML -= input.value - pSlider.value;
-    if (pAva.innerHTML < pSlider.min) {
-        pAva.innerHTML = pSlider.min;
-        input.value = pSlider.max;
+    var slider = document.getElementById(slider);
+    var total = document.getElementById(total);
+    var material = document.getElementById(formName);
+    total.innerHTML = input.value;
+    material.value = input.value;
+
+    // Detection for invalid input
+    //When input is over the maximum
+    if (total.innerHTML > slider.max) {
+        total.innerHTML = slider.max;
+        input.value = slider.max;
     }
-
-    pSlider.value = input.value;
+    //When input is below the minimum
+    if (total.innerHTML < slider.min){
+        total.innerHTML = slider.min;
+        input.value = slider.min;
+    }
+    slider.value = input.value;
+    totalCalculator();
 }
 
-function slider(slider,input,ava) {
-    var total = document.getElementById("total");
-    var pInput = document.getElementById(input);
-    var pAva = document.getElementById(ava);
-    pAva.innerHTML -= slider.value - pInput.value;
-    total.innerHTML = parseInt(total.innerHTML) + parseInt(slider.value) - parseInt(pInput.value);
-    pInput.value = slider.value;
+function slider(slider,input,total,formName) {
+    var input = document.getElementById(input);
+    var total = document.getElementById(total);
+    var material = document.getElementById(formName);
+    total.innerHTML = slider.value;
+    total.innerHTML = parseInt(total.innerHTML) + parseInt(slider.value) - parseInt(input.value);
+    input.value = slider.value;
+    material.value = slider.value;
+    totalCalculator();
 }
+
+function totalCalculator(){
+    var total = document.getElementById('totalForm');
+    var paper = document.getElementById('paperForm');
+    var plastic = document.getElementById('plastic');
+    var metal = document.getElementById('metalForm');
+    var glass = document.getElementById('glassForm');
+
+    total.value = paper.value + plastic.value + metal.value + glass.value;
+}
+
+
