6478c3c62954448566c4ac887a77b9d76aa64db8
Sayyaf Waseem <s.waseem@student.unimelb.edu.au>
2018-05-11 17:08:10 +1000

    table checking create element

--- site/js/Friends.js
+++ site/js/Friends.js
@@ -1,8 +1,11 @@
 function createProfilePic(source){
-    var img = document.createElement("img")
-    img.src = source;
-    img.align="center"
-    img.width="100%"
+    //var img = document.createElement("img")
+    //img.src = source;
+    //img.align="center"
+    //img.width="100%"
+    var para = document.createElement("P");
+    var t = document.createTextNode("HELLO FRIENDS");
+        para.appendChild(t);
     var element = document.getElementById('row');
-    element.appendChild(img);
+    element.appendChild(para);
 }
