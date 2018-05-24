75f218bc857e8cad53c092f53a1c0270cf3723ed
Sayyaf Waseem <s.waseem@student.unimelb.edu.au>
2018-05-24 10:35:44 +1000

    Added blog4

--- app.js
+++ app.js
@@ -59,6 +59,6 @@ app.get("/blog", function(req,res) {
 });
 
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3047;
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 exports.cookieCheck = cookieCheck;
